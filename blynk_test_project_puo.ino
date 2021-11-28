479806e78eec0801072afdd73e87f6635a5d8106
nafzaa94 <78883615+nafzaa94@users.noreply.github.com>
2021-11-28 22:49:30 +0800

    coding test blynk

---
+++ blynk_test_project_puo.ino
@@ -0,0 +1,45 @@
+#define BLYNK_TEMPLATE_ID "TMPLgihD0BWQ"
+#define BLYNK_DEVICE_NAME "TEST ULTRASONIC"
+#define BLYNK_AUTH_TOKEN "qDgRws8jt0MGBRkSBWNJunAaje6kcyQq"
+#define BLYNK_PRINT Serial
+
+#include <ESP8266_Lib.h>
+#include <BlynkSimpleShieldEsp8266.h>
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "YourNetworkName";
+char pass[] = "YourPassword";
+
+// Hardware Serial on Mega, Leonardo, Micro...
+//#define EspSerial Serial1
+
+// or Software Serial on Uno, Nano...
+#include <SoftwareSerial.h>
+SoftwareSerial EspSerial(2, 3); // RX, TX
+
+// Your ESP8266 baud rate:
+#define ESP8266_BAUD 9600
+
+ESP8266 wifi(&EspSerial);
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  delay(10);
+
+  // Set ESP8266 baud rate
+  EspSerial.begin(ESP8266_BAUD);
+  delay(10);
+
+  Blynk.begin(auth, wifi, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+}
